d068007c37ced2bf4d96ab449282d42ef19bbcfd
Jyrki Ahpola <jyrkia@gmail.com>
2020-01-04 13:34:14 +0200

    Initial setup

--- src/main/java/org/kerminator/portti/PorttiApplication.java
+++ src/main/java/org/kerminator/portti/PorttiApplication.java
@@ -1,28 +1,13 @@
 package org.kerminator.portti;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
-import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PorttiApplication {
 
-    @Autowired
-	private DiscoveryClient discoveryClient;
-	
-	@Bean
-    public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(DiscoveryClient discoveryClient,
-                                                                             DiscoveryLocatorProperties properties) {
-        return new DiscoveryClientRouteDefinitionLocator(discoveryClient,
-                                                         properties);
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(PorttiApplication.class, args);
 	}
