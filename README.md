243619e2762e5580257888c184c5f07bd4950998
Jyrki Ahpola <jyrki.ahpola@futurice.com>
2024-04-11 09:41:17 +0300

    Initial setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# spring-portti
+
+Spring Cloud Gateway example using kubernetes discovery via service labels
+
+## installation
+1. uses gradlew
+2. install some local kubernetes 
+3. run skaffold dev
+
+
+### Neat commands
+show gateway routes:
+```curl localhost:9000/actuator/gateway/routes```
---
+++ src/main/java/org/kerminator/portti/PorttiApplication.java
@@ -0,0 +1,17 @@
+package org.kerminator.portti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
+public class PorttiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorttiApplication.class, args);
+	}
+
+}
